e68adb6a51877b96eccec27e805850469f647898
aerraj <61013804+aerraj@users.noreply.github.com>
2023-04-18 18:44:39 +0530

    new files

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -5,4 +5,16 @@ export default async function handler(
     req:NextApiRequest,
     res:NextApiResponse
 )
+{
+    if(req.method!=='GET')
+    {
+        return res.status(405).end();
+    }
+    try{
+
+    }catch(error){
+        console.log(error);
+
+    }
+}
     
