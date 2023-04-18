81a9e5acc08db50feb8a9d4c82935432efd12957
aerraj <61013804+aerraj@users.noreply.github.com>
2023-04-18 18:48:43 +0530

    new improvements

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+import prisma from "@/libs/prismadb"
+
 export default async function handler(
 
     req:NextApiRequest,
@@ -12,9 +14,16 @@ export default async function handler(
     }
     try{
 
+        const users= await prisma.user.findMany({
+            orderBy:{
+                createdAt:'desc'
+            }
+        })
+return res.status(200).json(users);
+
     }catch(error){
         console.log(error);
-
+return res.status(400).end();
     }
 }
     
